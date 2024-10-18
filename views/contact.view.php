16803e945c1c0132dda1948bc58886104cc7df2a
BradleyTim <timbradley94@gmail.com>
2024-10-18 21:06:45 +0300

    add about and contact pages and create the views directory

---
+++ views/contact.view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notes App</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div>
+                <a href="/">Logo</a>
+            </div>
+            <div>
+                <a href="/about.php">About</a>
+                <a href="#">Notes</a>
+                <a href="/contact.php">Contact</a>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <h1>This is the CONTACT page</h1>
+        </section>
+    </main>
+    <footer>
+        <section>
+            Copyright &copy; 2024
+        </section>
+    </footer>
+</body>
+</html>
