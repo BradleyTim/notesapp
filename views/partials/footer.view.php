818c8083e81ce0160c42d7f93f16071b50ba22f3
BradleyTim <timbradley94@gmail.com>
2024-10-19 20:59:05 +0300

    add partials to avoid repetition

---
+++ views/partials/footer.view.php
@@ -0,0 +1,7 @@
+<footer>
+    <section>
+        Copyright &copy; 2024
+    </section>
+    </footer>
+</body>
+</html>
---
+++ views/partials/nav.view.php
@@ -0,0 +1,10 @@
+<nav>
+    <div>
+        <a href="/">Logo</a>
+    </div>
+    <div>
+        <a href="/about.php">About</a>
+        <a href="#">Notes</a>
+        <a href="/contact.php">Contact</a>
+    </div>
+</nav>
