f2907698ad182463b447b2d34737e9fb99a11b49
BradleyTim <timbradley94@gmail.com>
2024-10-19 20:59:56 +0300

    update pages to require partials

--- views/about.view.php
+++ views/about.view.php
@@ -1,32 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Notes App</title>
-</head>
-<body>
-    <header>
-        <nav>
-            <div>
-                <a href="/">Logo</a>
-            </div>
-            <div>
-                <a href="/about.php">About</a>
-                <a href="#">Notes</a>
-                <a href="/contact.php">Contact</a>
-            </div>
-        </nav>
-    </header>
-    <main>
-        <section>
-            <h1>This is the about page</h1>
-        </section>
-    </main>
-    <footer>
-        <section>
-            Copyright &copy; 2024
-        </section>
-    </footer>
-</body>
-</html>
+<?php require "partials/header.view.php"; ?>
+<header>
+    <?php require "partials/nav.view.php"; ?>
+</header>
+<main>
+    <section>
+        <h1>This is the about page</h1>
+    </section>
+</main>
+<?php require "partials/footer.view.php"; ?>
