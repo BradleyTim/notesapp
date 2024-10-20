d4e90d5588ff68a34f626e473acc0a0521abe35b
BradleyTim <timbradley94@gmail.com>
2024-10-20 21:55:25 +0300

    refactor router func

--- index.php
+++ index.php
@@ -11,12 +11,16 @@ $routes = [
 $route = strval(url_is());
 
 function route_to_key($route, $routes) {
-    if ($route === '/') {
-        require $routes['/'];
-    } elseif ($route === '/about') {
-        require $routes['/about'];
-    } else {
-        require $routes['/contact'];
+    // if ($route === '/') {
+    //     require $routes['/'];
+    // } elseif ($route === '/about') {
+    //     require $routes['/about'];
+    // } else {
+    //     require $routes['/contact'];
+    // }
+
+    if (array_key_exists($route, $routes)) {
+        require $routes[$route];
     }
 }
 
