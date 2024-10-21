a015ede3f04086db6268d28557fde619335b06f6
BradleyTim <timbradley94@gmail.com>
2024-10-21 08:23:29 +0300

    refactor funcs

--- functions.php
+++ functions.php
@@ -1,10 +1,12 @@
 <?php 
 
-function url_is() {
-    return parse_url($_SERVER['REQUEST_URI'])['path'];
+function url_is($value) {
+    return parse_url($_SERVER['REQUEST_URI'])['path'] === $value;
 }
 
 function dd($value) {
+    echo '<prev>';
     var_dump($value);
+    echo '</prev>';
     die();
 }
