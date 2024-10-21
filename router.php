ae143eae3623857009ec802ecb0034b9643a791f
BradleyTim <timbradley94@gmail.com>
2024-10-21 08:59:35 +0300

    move everything regarding routing to router.php

---
+++ router.php
@@ -0,0 +1,25 @@
+$routes = [
+    '/' => './controllers/index.php',
+    '/about' => './controllers/about.php',
+    '/contact' => './controllers/contact.php',
+];
+
+$route = strval(url());
+
+function abort($code=404) {
+    http_response_code($code);
+
+    require "/views/{$code}.php";
+
+    die();
+}
+
+function route_to_controller($route, $routes) {
+    if (array_key_exists($route, $routes)) {
+        require $routes[$route];
+    } else {
+        abort(404);
+    }
+}
+
+route_to_controller($route, $routes);
