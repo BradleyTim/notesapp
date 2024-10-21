11c2290a087a25d053b004a6855a64edf9b9f3b9
BradleyTim <timbradley94@gmail.com>
2024-10-21 08:46:47 +0300

    add 404 not found status code page

--- index.php
+++ index.php
@@ -13,6 +13,12 @@ $route = strval(url());
 function route_to_key($route, $routes) {
     if (array_key_exists($route, $routes)) {
         require $routes[$route];
+    } else {
+        http_response_code(404);
+
+        echo 'Sorry! Page not found. Go back to Home.';
+
+        die();
     }
 }
 
