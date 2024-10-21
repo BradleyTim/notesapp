1e85b4f4ddcb470180d293b5bdc7a693cb3d0af8
BradleyTim <timbradley94@gmail.com>
2024-10-21 08:52:25 +0300

    add abort func and the corresponding 404 page

--- index.php
+++ index.php
@@ -10,15 +10,19 @@ $routes = [
 
 $route = strval(url());
 
+function abort($code=404) {
+    http_response_code($code);
+
+    require "/views/{$code}.php";
+
+    die();
+}
+
 function route_to_key($route, $routes) {
     if (array_key_exists($route, $routes)) {
         require $routes[$route];
     } else {
-        http_response_code(404);
-
-        echo 'Sorry! Page not found. Go back to Home.';
-
-        die();
+        abort(404);
     }
 }
 
---
+++ views/404.php
@@ -0,0 +1,13 @@
+<?php require "partials/header.view.php"; ?>
+<header>
+    <?php require "partials/nav.view.php"; ?>
+</header>
+<main>
+    <section>
+        <h1>Sorry. Page not found.</h1>
+        <div>
+            <a href="/">Go back Home</a>
+        </div>
+    </section>
+</main>
+<?php require "partials/footer.view.php"; ?>
