fb145181db8bc115ccf9b46a68d74a548faba4f2
BradleyTim <timbradley94@gmail.com>
2024-10-31 20:40:23 +0300

    implement db class

--- DB.php
+++ DB.php
@@ -1,11 +1,22 @@
 <?php 
 
 class DB {
-    public static function query() {
-        $pdo = new PDO('sqlite:/Users/macbook/Code/notesapp/database/database.sqlite');
-        $statement = $pdo->prepare('select * from posts');
+    public $pdo;
+    public function __construct() {
+        $this->pdo = new PDO('sqlite:/Users/macbook/Code/notesapp/database/database.sqlite');
+    }
+    public function query() {
+        
+        $statement = $this->pdo->prepare('select * from posts');
         $statement->execute();
-        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-        return $result;
+        return $statement;
+    }
+
+    public function fetchall() {
+        return $this->query()->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function fetch() {
+        return $this->query()->fetch(PDO::FETCH_ASSOC);
     }
 }
