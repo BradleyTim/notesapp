8b9b3f8b01116cac7ff95973f41144848fa26667
BradleyTim <timbradley94@gmail.com>
2024-10-31 20:46:19 +0300

    move index.php and styles to public dir

--- public/index.php
+++ public/index.php
@@ -4,13 +4,3 @@ require __DIR__ . "/../functions.php";
 
 require base_dir('/router.php');
 // require './../router.php';
-
-require "DB.php";
-
-$db = new DB();
-
-$result = DB::query();
-// dd($result);
-foreach ($result as $post) {
-    echo "<li>{$post['title']}: {$post['body']}</li>";
-}
