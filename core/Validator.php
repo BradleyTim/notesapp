8470b3a78bc5676608e7aa01701adc70de880c07
BradleyTim <timbradley94@gmail.com>
2024-11-18 20:31:57 +0300

    update Validator class with string and email validation

--- core/Validator.php
+++ core/Validator.php
@@ -1,7 +1,11 @@
 <?php 
 
 class Validator {
-    public static function str($str) {
-        return strlen(trim($str)) > 0;
+    public static function string($str, $min, $max) {
+        return strlen(trim($str)) > $min && strlen(trim($str)) < $max;
+    }
+
+    public static function email($str) {
+        return filter_var($str, FILTER_VALIDATE_EMAIL) !== false;
     }
 }
