da4d87c341c422115f7d4d7a6a622bed61ff8a46
BradleyTim <timbradley94@gmail.com>
2024-11-18 20:52:27 +0300

    add errors and update ui to show errors

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -5,8 +5,18 @@ require base_dir("/DB.php");
 $config = require base_dir("/config.php");
 $db = new DB($config['database']['dsn']); 
 
+$errors = [];
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (Validator::string($_POST['title'], 1, 255) && Validator::string($_POST['body'], 1, 1000)) {
+    if (!Validator::string($_POST['title'], 1, 255)) {
+        $errors['title'] = 'A title of not more than 255 characters must be provided';
+    }
+
+    if(!Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of not more than 1000 characters must be provided';
+    }
+
+    if (empty($errors)) {
         $title = $_POST['title'];
         $body = $_POST['body'];
         $query = "insert into posts (title, body) values (:title, :body)";
@@ -14,6 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header('location:/');
         exit();
     }
+
 }
 
 require base_dir("/views/create.view.php");
--- views/create.view.php
+++ views/create.view.php
@@ -8,9 +8,11 @@
         <form action="" method="POST">
             <div>
                 <input type="text" name="title" id="title" placeholder="Title..." required>
+                <p><?= $errors['title'] ?? '' ?></p>
             </div>
             <div>
                 <input type="text" name="body" id="body" placeholder="what's on your mind? " required>
+                <p><?= $errors['body'] ?? '' ?></p>
             </div>
             <div>
                 <button type="submit">Create Note</button>
