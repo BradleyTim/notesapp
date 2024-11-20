9622d2ebc10ca705a45d107a633f2ecb8e9524eb
BradleyTim <timbradley94@gmail.com>
2024-11-20 21:05:35 +0300

    move css from the head of the html doc to the css dir and import the styles

--- views/partials/header.view.php
+++ views/partials/header.view.php
@@ -4,68 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notes App</title>
-    <!-- <link rel="stylesheet" href="/../../public/css/style.css"> -->
-     <style>
-         * {
-            box-sizing: border-box;
-         }
-
-         body {
-            height:100vh;
-            display:grid;
-            grid-template-rows: auto 1fr auto;
-         }
-
-         header, main, footer {
-            width:920px;
-            margin-inline:auto;
-         }
-
-         nav {
-            display: flex;
-            justify-content: space-between;
-         }
-        details {
-        border: 1px solid #aaa;
-        border-radius: 4px;
-        padding: 1em 1em;
-        margin-block: 1em;
-        max-width: 920px;
-
-        }
-
-        summary {
-        font-weight: bold;
-        margin: -0.5em -0.5em 0;
-        padding: 0.5em;
-        }
-
-        details[open] {
-        padding: 0.5em;
-        }
-
-        details[open] summary {
-        border-bottom: 1px solid #aaa;
-        margin-bottom: 0.5em;
-        }
-
-        div {
-         margin-block: 1rem; 
-        }
-
-        input, button {
-         padding: 0.5em;
-         border: 1px solid #ccc;
-         font-size: 1em;
-         line-height: 1.5;
-         border-radius: 0.5em;
-         width: 920px;
-        }
-
-        button {
-         background-color: aqua; 
-        }
-
-     </style>
+    <link rel="stylesheet" href="/css/style.css"> 
 </head>
 <body>
