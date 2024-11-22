3c84f22d7feab23aeaeff947e2dbd4d7e193b7fd
BradleyTim <timbradley94@gmail.com>
2024-11-22 20:42:33 +0300

    add nav styles

--- views/partials/nav.view.php
+++ views/partials/nav.view.php
@@ -2,7 +2,7 @@
     <div>
         <a href="/">Logo</a>
     </div>
-    <div>
+    <div class="menu">
         <a href="/about">About</a>
         <a href="/notes">Notes</a>
         <a href="/contact">Contact</a>
