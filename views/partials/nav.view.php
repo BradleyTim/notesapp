8d1731585fd0fd5c57ea3506067c3e3ace908ef3
BradleyTim <timbradley94@gmail.com>
2024-11-24 19:44:20 +0300

    update nav menu

--- views/partials/nav.view.php
+++ views/partials/nav.view.php
@@ -3,8 +3,8 @@
         <a href="/">Logo</a>
     </div>
     <div class="menu">
+        <a href="/notes/create">Create Note</a>
         <a href="/about">About</a>
-        <a href="/notes">Notes</a>
         <a href="/contact">Contact</a>
     </div>
 </nav>
