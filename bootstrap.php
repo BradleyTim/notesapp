81e1f3ba59cf6bca70c6cd7cd108aec3bb04d5bd
BradleyTim <timbradley94@gmail.com>
2024-11-26 22:07:41 +0300

    create bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,15 @@
+<?php 
+
+use Core\App;
+use Core\Container; 
+use Core\DB;
+
+$container = new Container();
+$container->bind('Core/DB', function() {
+    $config = require base_dir("/config.php");
+    $db = new DB($config['database']['dsn']); 
+    return $db;
+});
+
+//$db = $container->resolve('Core/DB'); 
+App::setContainer($container);
